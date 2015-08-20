8de3ce75999d61ebbf2d7092b1392e8d9305655e
Javier G. Sogo <jgsogo@gmail.com>
2015-08-20 12:49:23 +0200

    up version

--- apicultur/__init__.py
+++ apicultur/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 from .apicultur import Apicultur
 from .service import UnauthorizedError, UnhandledError, RateLimitError
