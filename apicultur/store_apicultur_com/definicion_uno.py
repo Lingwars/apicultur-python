7102c0ddeabc0bb1834684aa9620a1a12b123223
ebaste <ebaste@gmail.com>
2015-08-26 20:11:12 +0200

    Corregida Efinicion

--- apicultur/store_apicultur_com/definicion_uno.py
+++ apicultur/store_apicultur_com/definicion_uno.py
@@ -4,7 +4,7 @@
 from apicultur.service import Service
 # https://store.apicultur.com/apis/info?name=DiccionariodeEspanol&version=1.0.0&provider=MolinodeIdeas
 # https://store.apicultur.com/api/dicc/1.0.0/definicion/10/mel%C3%B3n
-class Eefinicion_Uno(Service):
+class Definicion_Uno(Service):
     prepoint = 'dicc'
     version = '1.0.0'
     endpoint = 'definicion/10'
