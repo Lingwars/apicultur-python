68084442d25c080797ab40bea6845d469221e11e
Javier G. Sogo <jgsogo@gmail.com>
2015-09-14 16:42:26 +0200

    implementing analizador_sintactico

--- apicultur/store_apicultur_com/silabeador_ortografico.py
+++ apicultur/store_apicultur_com/silabeador_ortografico.py
@@ -3,7 +3,7 @@
 
 from apicultur.service import Service
 
-class CalculadorFrecuencia(Service):
+class SilabeadorOrtografico(Service):
     # http://store.apicultur.com/apis/info?name=SilabeadorOrtografico&version=1.0.0&provider=MolinodeIdeas
     version = '1.0.0'
     endpoint = 'silabeame'
