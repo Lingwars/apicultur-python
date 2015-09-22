6f4e45ba5d42fb55ee8ca483d2d72b8a4d444fa0
Javier G. Sogo <jgsogo@gmail.com>
2015-09-22 10:14:23 +0200

    fixes minor typos

--- apicultur/apicultur_io/transitive_verb.py
+++ apicultur/apicultur_io/transitive_verb.py
@@ -3,13 +3,13 @@
 
 from apicultur.service import Service
 
-class PhoneToEnglish(Service):
+class TransitiveVerb(Service):
     # # http://apicultur.io/apis/info?name=VerbTransitive_Onoma_es&version=1.0.0&provider=molinodeideas
     version = '1.0.0'
     endpoint = 'onoma/conjugator/es/transitiveverb'
     method = 'GET'
     arguments = ['infinitivo',]
-    func_name = 'transitive-verb'
+    func_name = 'transitive_verb'
 
     def get_endpoint(self):
-        return self._join_url(self.endpoint, self.version, '?word=%(word)s')
+        return self._join_url(self.endpoint, self.version, '?infinitivo=%(infinitivo)s')
